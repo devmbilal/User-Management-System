1319280fddde6a835534cd6241e5be9ede4e2003
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-09-03 12:23:24 +0500

    dependencies added

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+ 
+ // Static Fields 
+app.use(express.static('public'));
+
+// Templating Engine   
+app.use(expressLayouts);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+
